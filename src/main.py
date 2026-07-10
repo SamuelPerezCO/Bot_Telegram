d7f8485c522c1c7bfb2b7f41f1099b2e0966d4cb
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-10 16:41:23 -0500

    Changes

--- src/main.py
+++ src/main.py
@@ -6,14 +6,11 @@ import os
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+application = ApplicationBuilder().token(BOT_TOKEN).build()
+
 async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello World!")
     
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
-
-application = ApplicationBuilder().token(BOT_TOKEN).build()
-
 application.add_handler(CommandHandler("start" , say_hello ))
-application.add_handler(CommandHandler("echo" , echo))
+
 application.run_polling(allowed_updates=Update.ALL_TYPES)
