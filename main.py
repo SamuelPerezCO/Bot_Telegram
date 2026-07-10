5496a8fed6cae19f5db90752c5e7ea6ed4f8ebe4
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-10 17:21:11 -0500

    Handler of add

--- main.py
+++ main.py
@@ -10,14 +10,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello World!")
-
-async def search(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(context.args)
-    await update.message.reply_text( context.args[0])
-    
-application.add_handler(CommandHandler("start" , say_hello ))
-application.add_handler(CommandHandler("search" , search ))
-
+application.add_handler(CommandHandler("add" , TodoController.add_todo))
 application.run_polling(allowed_updates=Update.ALL_TYPES)
