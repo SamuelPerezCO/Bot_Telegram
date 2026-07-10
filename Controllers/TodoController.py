11bafbd00c7d02cde46b658c9726c6bf25215018
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-10 17:40:55 -0500

    List todo

--- Controllers/TodoController.py
+++ Controllers/TodoController.py
@@ -1,10 +1,24 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from Models.Todo import Todo
+from Models.TodoList import todo_list
+
 class TodoController:
 
     @staticmethod
     async def add_todo(update: Update , context: ContextTypes.DEFAULT_TYPE):
-        text_splitted = update.message.text.split()
         command = update.message.text.split()[0]
-        print(text_splitted)
+        title = "".join(update.message.text.split(command)[1])
+        todo_list.append( Todo(title) )
+        await update.message.reply_text("Nota agregada!")
+
+    @staticmethod
+    async def list_todo(update: Update , context: ContextTypes.DEFAULT_TYPE):
+        if (len(todo_list) == 0):
+            await update.message.reply_text("No hay tareas todavia")
+            return
+        answer = ""
+        for i , todo in enumerate(todo_list):
+            answer = answer + f"{i + 1} - {'O' if todo.is_completed else 'X'} {todo.title}"
+        await update.message.reply_text(answer)
