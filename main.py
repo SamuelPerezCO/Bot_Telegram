7133596661ded43f749f60753a0f8f82acbaf1c5
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-10 17:52:36 -0500

    Check

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 application = ApplicationBuilder().token(BOT_TOKEN).build()
 
-application.add_handler(CommandHandler("add" , TodoController.add_todo))
-application.add_handler(CommandHandler("list" , TodoController.list_todo))
+application.add_handler( CommandHandler("add" , TodoController.add_todo ) )
+application.add_handler( CommandHandler("list" , TodoController.list_todo ) )
+application.add_handler( CommandHandler("check" , TodoController.check_todo ) )
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
