00ae3290a38befafc36d0a2547050d0183b76f7d
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-10 17:22:04 -0500

    Adding the async

--- Controllers/TodoController.py
+++ Controllers/TodoController.py
@@ -4,7 +4,7 @@ from telegram.ext import ContextTypes
 class TodoController:
 
     @staticmethod
-    def add_todo(update: Update , context: ContextTypes.DEFAULT_TYPE):
+    async def add_todo(update: Update , context: ContextTypes.DEFAULT_TYPE):
         text_splitted = update.message.text.split()
         command = update.message.text.split()[0]
         print(text_splitted)
