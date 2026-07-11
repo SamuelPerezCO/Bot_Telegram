d29827caa7ea51a065cf4d6fc7ae9db6d96de05a
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-11 10:14:04 -0500

    Command handler correction with claude

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes , MessageHandler , filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes , MessageHandler , filters , ConversationHandler
 from dotenv import load_dotenv
 from telegram import Update
 import os
@@ -15,7 +15,15 @@ application.add_handler( CommandHandler("list" , TodoController.list_todo ) )
 application.add_handler( CommandHandler("start" , TodoController.say_hello ) )
 application.add_handler( CommandHandler("check" , TodoController.check_todo ) )
 application.add_handler( CommandHandler("clear" , TodoController.clear_todos ) )
+application.add_handler( MessageHandler(filters.TEXT & ~filters.COMMAND, TodoController.send_message))
 
-application.add_handler( MessageHandler(filters.TEXT & ~filters.AUDIO, TodoController.send_message))
+info_conversation_handler = ConversationHandler(
+    entry_points=[ CommandHandler("data" , TodoController.ask_name)],
+    states={
+
+    },
+    fallbacks=[]
+)
+application.add_handler(info_conversation_handler)
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
