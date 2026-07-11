8e88111bc8a3214569cf1ee5dcf9c83c5a0213dd
SamuelPerezCO <sernasamuelperez@gmail.com>
2026-07-11 11:07:07 -0500

    Message Handler for GET_NAME and GET_LAST_NAME

--- main.py
+++ main.py
@@ -15,15 +15,16 @@ application.add_handler( CommandHandler("list" , TodoController.list_todo ) )
 application.add_handler( CommandHandler("start" , TodoController.say_hello ) )
 application.add_handler( CommandHandler("check" , TodoController.check_todo ) )
 application.add_handler( CommandHandler("clear" , TodoController.clear_todos ) )
-application.add_handler( MessageHandler(filters.TEXT & ~filters.COMMAND, TodoController.send_message))
-
 info_conversation_handler = ConversationHandler(
     entry_points=[ CommandHandler("data" , TodoController.ask_name)],
     states={
-
+        TodoController.GET_NAME: [ MessageHandler(filters.TEXT & ~filters.COMMAND, TodoController.get_name) ],
+        TodoController.GET_LAST_NAME: [ MessageHandler(filters.TEXT & ~filters.COMMAND, TodoController.get_last_name) ],
     },
     fallbacks=[]
 )
 application.add_handler(info_conversation_handler)
+
+application.add_handler( MessageHandler(filters.TEXT & ~filters.COMMAND, TodoController.send_message))
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
